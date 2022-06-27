a2f0876decd57274e0e5cf56ac1f24320068142c
Mosherof <mosherof@ucsd.edu>
2022-06-27 11:19:32 -0700

    added newlines, fixed typos.

--- generate_example_plot.py
+++ generate_example_plot.py
@@ -5,13 +5,14 @@ import numpy as np
 
 def make_data():
     #Generates Random CGM and and Reference data
-    #Creates an array of 300 data points and uses log scale to create a clustering 
+    #Creates an array of about 300 data points and uses log scale to create a clustering 
     cgm=[np.random.uniform(0,400)/math.log(n+2) + 45 for n in range(300)]
     cgm_temp=[]
     for i in cgm:
         if i < 400:
             cgm_temp.append(i)
     cgm=cgm_temp
+    
     #Adds noise to CGM array to create reference
     reference = []
     for n in range(len(cgm)):
